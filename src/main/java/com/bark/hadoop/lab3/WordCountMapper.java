8614302c2f60d633de0e88981723b7397ac7ead8
roozbeh <roozbeh@RoozMint>
2015-10-05 19:39:18 -0400

    Added Mapper (needs checking and renaming).

--- src/main/java/com/bark/hadoop/lab3/WordCountMapper.java
+++ src/main/java/com/bark/hadoop/lab3/WordCountMapper.java
@@ -7,26 +7,68 @@ package com.bark.hadoop.lab3;
 /**
  * Credits: http://log.malchiodi.com/2014/11/12/executing-jar-encoded-mapreduce-jobs-in-aws-either-through-web-interface-or-cli/
  */
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import java.util.StringTokenizer;
+import javax.xml.stream.XMLInputFactory;
+import static javax.xml.stream.XMLStreamConstants.CHARACTERS;
+import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
+import javax.xml.stream.XMLStreamReader;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
-    private final IntWritable one = new IntWritable(1);
-    private Text word = new Text();
 
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        StringTokenizer itr = new StringTokenizer(line.toLowerCase(), " .,:;!?{}[]()|_");
-        while (itr.hasMoreTokens()) {
-            word.set(itr.nextToken());
-            context.write(word, one);
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+    protected void map(LongWritable key, Text value, Mapper.Context context)
+          throws IOException, InterruptedException 
+    {
+        String document = value.toString();
+       // System.out.println("‘" + document + "‘");
+        try
+        {
+            XMLStreamReader reader =
+              XMLInputFactory.newInstance().createXMLStreamReader(new
+                  ByteArrayInputStream(document.getBytes()));
+            String Title = "";
+            String Links = "";
+            String TextData = "";
+            String currentElement = "";
+            while (reader.hasNext()) {
+              int code = reader.next();
+              switch (code) {
+                case START_ELEMENT:
+                  currentElement = reader.getLocalName();
+                  break;
+                case CHARACTERS:
+                  if (currentElement.equalsIgnoreCase("title")) {
+                    Title += reader.getText();
+                  }else if (currentElement.equalsIgnoreCase("text"))
+                  {
+                      TextData += reader.getText();
+                  }
+                  break;
+              }
+            }
+            reader.close();
+            
+            Title =  Title.replace(" ", "_");
+            Pattern p = Pattern.compile("[[(.*)]]");//TODO: Check if valid!
+            Matcher m = p.matcher(TextData);
+            for(int i = 0;i<m.groupCount();i++)
+                Links += " " + m.group(i);
+            context.write(Title, Links);
         }
-    }
+        catch (Exception e)
+        {
+            //TODO: do something?
+        }
+      }
 }
